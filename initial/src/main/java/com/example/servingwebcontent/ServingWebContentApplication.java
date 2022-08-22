1f46bbdba79d5bf56e1c58977998e1a200119e72
Igor Pets <b_ear@mail.ru>
2022-08-22 09:21:22 +0300

    WEB link

--- initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * https://spring-projects.ru/guides/serving-web-content/
+ * https://habr.com/ru/post/478402/?ysclid=l74d7n4ugx270529461
+ * https://habr.com/ru/post/479286/?ysclid=l74dd1khx5848160990
  * */
 
 @SpringBootApplication
