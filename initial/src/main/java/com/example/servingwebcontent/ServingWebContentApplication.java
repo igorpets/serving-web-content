e0912334c797367915319944adf5653c13db9ea5
Igor Pets <b_ear@mail.ru>
2022-08-22 09:09:47 +0300

    Working version

--- initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -2,12 +2,13 @@ package com.example.servingwebcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * https://spring-projects.ru/guides/serving-web-content/
+ * */
 
 @SpringBootApplication
 public class ServingWebContentApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServingWebContentApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
 }
